12d9a322d73752d04155e66d9a41867c1de53683
Jiri Srba <jiri.srba@gmail.com>
2017-12-30 16:32:53 +0100

    Test subprocess

--- test.py
+++ test.py
@@ -5,27 +5,18 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import sys
+import subprocess
 import yaml
 
 
 def main(argv):
+  """Main()"""
+  cmd = 'echo blabla'
+  output = subprocess.Popen(cmd, shell=True, close_fds=True)
+  print("output Popen: {}".format(output))
 
-  config_file = 'oracle-ci.yml'
-
-  s = 'jirka,boson'
-
-  try:
-    with open(config_file, 'r') as stream:
-      cfg = yaml.load(stream)
-      print(cfg)
-      s = cfg['variables']['dbname']
-      if not isinstance(s, list):
-        s = ''.join(c if str.isalnum(c) else ' ' for c in s).split()
-      print(type(s))
-      print(s)
-  except (IOError, yaml.YAMLError) as exc:
-    print(exc)
-    raise
+  output = subprocess.check_output(cmd.split())
+  print("output check_output: {}".format(output))
 
 
 if __name__ == "__main__":
