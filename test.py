66c76449326f81694c2136c2d65b3534663628c9
Jiri Srba <jsrba@csas.cz>
2019-01-14 15:09:46 +0100

    Test

--- test.py
+++ test.py
@@ -6,21 +6,22 @@ from __future__ import print_function
 
 import sys
 import subprocess
-from collections import Counter
-import yaml
-
-
-def counter(a):
-  return sorted(Counter(a))
 
 
 def main(argv):
   """Main()"""
 
-  ora_errors = ["SP2-0003: Ill-formed ACCEPT command starting as LANSWER FORMAT A1 PROMPT 'Do you wish to continue anyway? (y\\n): '", 'ORA-00942: table or view does not exist', 'ORA-00942: table or view does not exist', 'ORA-00942: table or view does not exist']
-  for key, value in counter(ora_errors).items():
-    print(key, value)
+  cfg = {
+      'variables': {
+          'database': None,
+          'app': None,
+          'user': 'SYS'},
+      'stage': ['deploy'],
+      'script': [],
+      'jira': None}
 
+  if 'db' in cfg['variables']:
+    print('aa')
 
 if __name__ == "__main__":
   main(sys.argv[1:])
