b4c85e2b386c69fee6624db40822556d4dbf12ec
Jiri Srba <jsrba@csas.cz>
2019-03-06 18:33:14 +0100

    Add lib Adafruit_DHT

--- malina/akvarko.py
+++ malina/akvarko.py
@@ -6,13 +6,13 @@
 #
 
 python libs:
-   - https://github.com/szazo/DHT11_Python
+   - https://github.com/adafruit/Adafruit_Python_DHT
    - https://github.com/sourceperl/rpi.lcd-i2c
 
 """
 
 import time
-import dht11
+import Adafruit_DHT
 
 import RPi_I2C_LCD
 from RPi import GPIO
@@ -21,13 +21,12 @@ from w1thermsensor import W1ThermSensor
 # DEBUG flag
 DEBUG = False
 
-dht_temp_sensor = 18  #define GPIO 18 as DHT11 data pin
+dht11_pin = 18  #define GPIO 18 as DHT11 data pin
 ds_sensor_id = '0000055d2b41'  # 0000055d2b41
 
 
 def lcd_init():
-  """init LCD
-  """
+  """init LCD """
   lcd = RPi_I2C_LCD.LCD()
   lcd.set_backlight(True)
   lcd.clear()
@@ -45,12 +44,12 @@ def main():
   sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, ds_sensor_id)
 
   # DHT11
-  instance = dht11.DHT11(pin=dht_temp_sensor)
+  dht11_sensor_device = Adafruit_DHT.DHT11
 
   # LCD
   lcd = lcd_init()
 
-  temp = {
+  data = {
       'ds18b20': 0,
       'dht11':  0,
       'humi': 0}
@@ -59,26 +58,26 @@ def main():
   while True:
 
     # DS18B20
-    temp['ds18b20'] = round(sensor.get_temperature(), 1)
+    data['ds18b20'] = round(sensor.get_temperature(), 1)
 
     # DHT11
-    dht11_result = instance.read()
+    temp_dht11, humi_dht11 = Adafruit_DHT.read_retry(dht11_sensor_device,
+                                                     dht11_pin)
 
-    if dht11_result.is_valid():
-      temp['dht11'] = round(dht11_result.temperature, 1)
-      temp['humi'] = round(dht11_result.humidity, 1)
+    if humi_dht11 is not None and temp_dht11 is not None:
+      data['dht11'] = temp_dht11
+      data['humi'] = temp_dht11
 
     if DEBUG:
-      print("ds: {} dht: {}".format(temp['ds18b20'], temp['dht11']))
+      print("ds: {} dht: {}".format(data['ds18b20'], data['dht11']))
 
     # LCD line 1
-    lcd_line1 = "T1:{:.1f}C T2:{:.1f}C".format(
-        temp['ds18b20'], temp['dht11'])
+    lcd_line1 = "T1:{:.1f}C T2:{:.1f}C".format(data['ds18b20'], data['dht11'])
     lcd.set_cursor(row=0)
     lcd.message(lcd_line1)
 
     # LCD line 2
-    lcd_line2 = "humidity:{:.1f}%".format(temp['humi'])
+    lcd_line2 = "humidity:{:.1f}%".format(data['humi'])
     lcd.set_cursor(row=1)
     lcd.message(lcd_line2)
 
